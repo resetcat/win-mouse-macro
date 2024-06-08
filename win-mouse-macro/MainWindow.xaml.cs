9341edb46ce86173245b8f23ed6df7b68ef3602d
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2024-06-08 11:26:32 +0300

    batton layout p1

--- win-mouse-macro/MainWindow.xaml.cs
+++ win-mouse-macro/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace win_mouse_macro {
     /// <summary>
@@ -21,5 +8,17 @@ namespace win_mouse_macro {
         public MainWindow() {
             InitializeComponent();
         }
+
+
+        private void btnRecord_Click(object sender, RoutedEventArgs e) {
+            lstRecords.Items.Add("Recording started...");
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e) {
+            lstRecords.Items.Add("Playing back actions...");
+        }
+
     }
+
+
 }
